3d656789d5ed30ae00da6b5f5e901ec7a2707b05
jupitters <83190205+jupitters@users.noreply.github.com>
2024-06-17 12:13:36 -0300

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -5,87 +5,119 @@
 
 #define MAX_EXPR_SIZE 100
 #define MAX_TOKEN_SIZE 20
-#define MAX_STACK_SIZE 100
+#define MAX_Pilha_SIZE 100
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
 
 typedef struct {
-    double data[MAX_STACK_SIZE];
-    int top;
-} Stack;
+    double data[MAX_Pilha_SIZE];
+    int topo;
+} Pilha;
 
 typedef struct {
-    char *data[MAX_STACK_SIZE];
-    int top;
-} ExprStack;
+    char *data[MAX_Pilha_SIZE];
+    int topo;
+} ExprPilha;
 
-void init_stack(Stack *s) {
-    s->top = -1;
+void inicializarPilha(Pilha *p);
+void inicializarExprPilha(ExprPilha *p);
+int estaVazia(Pilha *p);
+int exprEstaVazia(ExprPilha *p);
+int estaCheia(Pilha *p);
+int exprEstaCheia(ExprPilha *p);
+void empilha(Pilha *p, double valor);
+void empilhaExpr(ExprPilha *p, char *valor);
+double desempilha(Pilha *p);
+char *desempilhaExpr(ExprPilha *p);
+double topo(Pilha *p);
+char *topoExpr(ExprPilha *p);
+double aplicarOperador(char op, double a, double b);
+double grausParaRadianos(double graus);
+double aplicarFuncao(const char *func, double a);
+double avaliarPosFixa(const char *expressao, char *infixa);
+
+int main() {
+    char expressao[MAX_EXPR_SIZE];
+    char infixa[MAX_EXPR_SIZE] = "";
+
+    printf("Digite a expressao em notacao pos-fixada: ");
+    fgets(expressao, MAX_EXPR_SIZE, stdin);
+    expressao[strcspn(expressao, "\n")] = '\0'; 
+
+    double resultado = avaliarPosFixa(expressao, infixa);
+    printf("Resultado: %.2f\n", resultado);
+    printf("Expressao Infixa: %s\n", infixa);
+
+    return 0;
 }
 
-void init_expr_stack(ExprStack *s) {
-    s->top = -1;
+void inicializarPilha(Pilha *p) {
+    p->topo = -1;
 }
 
-int is_empty(Stack *s) {
-    return s->top == -1;
+void inicializarExprPilha(ExprPilha *p) {
+    p->topo = -1;
 }
 
-int is_expr_empty(ExprStack *s) {
-    return s->top == -1;
+int estaVazia(Pilha *p) {
+    return p->topo == -1;
 }
 
-int is_full(Stack *s) {
-    return s->top == MAX_STACK_SIZE - 1;
+int exprEstaVazia(ExprPilha *p) {
+    return p->topo == -1;
 }
 
-int is_expr_full(ExprStack *s) {
-    return s->top == MAX_STACK_SIZE - 1;
+int estaCheia(Pilha *p) {
+    return p->topo == MAX_Pilha_SIZE - 1;
 }
 
-void push(Stack *s, double value) {
-    if (!is_full(s)) {
-        s->data[++s->top] = value;
+int exprEstaCheia(ExprPilha *p) {
+    return p->topo == MAX_Pilha_SIZE - 1;
+}
+
+void empilha(Pilha *p, double valor) {
+    if (!estaCheia(p)) {
+        p->data[++p->topo] = valor;
     }
 }
 
-void push_expr(ExprStack *s, char *value) {
-    if (!is_expr_full(s)) {
-        s->data[++s->top] = value;
+void empilhaExpr(ExprPilha *p, char *valor) {
+    if (!exprEstaCheia(p)) {
+        p->data[++p->topo] = valor;
     }
 }
 
-double pop(Stack *s) {
-    if (!is_empty(s)) {
-        return s->data[s->top--];
+double desempilha(Pilha *p) {
+    if (!estaVazia(p)) {
+        return p->data[p->topo--];
     }
     return 0;
 }
 
-char *pop_expr(ExprStack *s) {
-    if (!is_expr_empty(s)) {
-        return s->data[s->top--];
+char *desempilhaExpr(ExprPilha *p) {
+    if (!exprEstaVazia(p)) {
+        return p->data[p->topo--];
     }
     return NULL;
 }
 
-double top(Stack *s) {
-    if (!is_empty(s)) {
-        return s->data[s->top];
+double topo(Pilha *p) {
+    if (!estaVazia(p)) {
+        return p->data[p->topo];
     }
     return 0;
 }
 
-char *top_expr(ExprStack *s) {
-    if (!is_expr_empty(s)) {
-        return s->data[s->top];
+char *topoExpr(ExprPilha *p) {
+    if (!exprEstaVazia(p)) {
+        return p->data[p->topo];
     }
     return NULL;
 }
 
-double apply_operator(char op, double a, double b) {
+double aplicarOperador(char op, double a, double b) {
     switch (op) {
         case '+': return a + b;
         case '-': return a - b;
@@ -96,69 +128,69 @@ double apply_operator(char op, double a, double b) {
     }
 }
 
-double degrees_to_radians(double degrees) {
-    return degrees * M_PI / 180.0;
+double grausParaRadianos(double graus) {
+    return graus * M_PI / 180.0;
 }
 
-double apply_function(const char *func, double a) {
+double aplicarFuncao(const char *func, double a) {
     if (strcmp(func, "raiz") == 0) return sqrt(a);
-    if (strcmp(func, "sen") == 0) return sin(degrees_to_radians(a));
-    if (strcmp(func, "cos") == 0) return cos(degrees_to_radians(a));
-    if (strcmp(func, "tg") == 0) return tan(degrees_to_radians(a));
+    if (strcmp(func, "sen") == 0) return sin(grausParaRadianos(a));
+    if (strcmp(func, "cos") == 0) return cos(grausParaRadianos(a));
+    if (strcmp(func, "tg") == 0) return tan(grausParaRadianos(a));
     if (strcmp(func, "log") == 0) return log10(a);
     return 0;
 }
 
-double evaluate_postfix(const char *expression, char *infix) {
-    Stack s;
-    ExprStack es;
-    init_stack(&s);
-    init_expr_stack(&es);
+double avaliarPosFixa(const char *expressao, char *infixa) {
+    Pilha p;
+    ExprPilha ep;
+    inicializarPilha(&p);
+    inicializarExprPilha(&ep);
 
     char token[MAX_TOKEN_SIZE];
     int i = 0, j = 0;
 
-    while (expression[i] != '\0') {
-        if (expression[i] == ' ') {
+    while (expressao[i] != '\0') {
+        if (expressao[i] == ' ') {
             token[j] = '\0';
             if (j > 0) {
                 if (strchr("+-*/^", token[0]) && j == 1) {
-                    double b = pop(&s);
-                    double a = pop(&s);
-                    double result = apply_operator(token[0], a, b);
-                    push(&s, result);
+                    double b = desempilha(&p);
+                    double a = desempilha(&p);
+                    double resultado = aplicarOperador(token[0], a, b);
+                    empilha(&p, resultado);
 
-                    char *right = pop_expr(&es);
-                    char *left = pop_expr(&es);
-                    char *new_expr = malloc(MAX_EXPR_SIZE);
-                    snprintf(new_expr, MAX_EXPR_SIZE, "(%s %c %s)", left, token[0], right);
-                    push_expr(&es, new_expr);
-                    free(left);
-                    free(right);
+                    char *direita = desempilhaExpr(&ep);
+                    char *esquerda = desempilhaExpr(&ep);
+                    char *novaExpr = malloc(MAX_EXPR_SIZE);
+                    snprintf(novaExpr, MAX_EXPR_SIZE, "(%s %c %s)", esquerda, token[0], direita);
+                    empilhaExpr(&ep, novaExpr);
+                    free(esquerda);
+                    free(direita);
 
                 } else if (strcmp(token, "raiz") == 0 || strcmp(token, "sen") == 0 ||
                            strcmp(token, "cos") == 0 || strcmp(token, "tg") == 0 ||
                            strcmp(token, "log") == 0) {
-                    double a = pop(&s);
-                    double result = apply_function(token, a);
-                    push(&s, result);
+                    double a = desempilha(&p);
+                    double resultado = aplicarFuncao(token, a);
+                    empilha(&p, resultado);
 
-                    char *operand = pop_expr(&es);
-                    char *new_expr = malloc(MAX_EXPR_SIZE);
-                    snprintf(new_expr, MAX_EXPR_SIZE, "%s(%s)", token, operand);
-                    push_expr(&es, new_expr);
-                    free(operand);
+                    char *operando = desempilhaExpr(&ep);
+                    char *novaExpr = malloc(MAX_EXPR_SIZE);
+                    snprintf(novaExpr, MAX_EXPR_SIZE, "%s(%s)", token, operando);
+                    empilhaExpr(&ep, novaExpr);
+                    free(operando);
 
                 } else {
-                    push(&s, atof(token));
-                    char *operand = malloc(MAX_TOKEN_SIZE);
-                    snprintf(operand, MAX_TOKEN_SIZE, "%s", token);
-                    push_expr(&es, operand);
+                    empilha(&p, atof(token));
+                    char *operando = malloc(MAX_TOKEN_SIZE);
+                    snprintf(operando, MAX_TOKEN_SIZE, "%s", token);
+                    empilhaExpr(&ep, operando);
                 }
                 j = 0;
             }
         } else {
-            token[j++] = expression[i];
+            token[j++] = expressao[i];
         }
         i++;
     }
@@ -166,55 +198,40 @@ double evaluate_postfix(const char *expression, char *infix) {
     token[j] = '\0';
     if (j > 0) {
         if (strchr("+-*/^", token[0]) && j == 1) {
-            double b = pop(&s);
-            double a = pop(&s);
-            double result = apply_operator(token[0], a, b);
-            push(&s, result);
+            double b = desempilha(&p);
+            double a = desempilha(&p);
+            double resultado = aplicarOperador(token[0], a, b);
+            empilha(&p, resultado);
 
-            char *right = pop_expr(&es);
-            char *left = pop_expr(&es);
-            char *new_expr = malloc(MAX_EXPR_SIZE);
-            snprintf(new_expr, MAX_EXPR_SIZE, "(%s %c %s)", left, token[0], right);
-            push_expr(&es, new_expr);
-            free(left);
-            free(right);
+            char *direita = desempilhaExpr(&ep);
+            char *esquerda = desempilhaExpr(&ep);
+            char *novaExpr = malloc(MAX_EXPR_SIZE);
+            snprintf(novaExpr, MAX_EXPR_SIZE, "(%s %c %s)", esquerda, token[0], direita);
+            empilhaExpr(&ep, novaExpr);
+            free(esquerda);
+            free(direita);
 
         } else if (strcmp(token, "raiz") == 0 || strcmp(token, "sen") == 0 ||
                    strcmp(token, "cos") == 0 || strcmp(token, "tg") == 0 ||
                    strcmp(token, "log") == 0) {
-            double a = pop(&s);
-            double result = apply_function(token, a);
-            push(&s, result);
+            double a = desempilha(&p);
+            double resultado = aplicarFuncao(token, a);
+            empilha(&p, resultado);
 
-            char *operand = pop_expr(&es);
-            char *new_expr = malloc(MAX_EXPR_SIZE);
-            snprintf(new_expr, MAX_EXPR_SIZE, "%s(%s)", token, operand);
-            push_expr(&es, new_expr);
-            free(operand);
+            char *operando = desempilhaExpr(&ep);
+            char *novaExpr = malloc(MAX_EXPR_SIZE);
+            snprintf(novaExpr, MAX_EXPR_SIZE, "%s(%s)", token, operando);
+            empilhaExpr(&ep, novaExpr);
+            free(operando);
 
         } else {
-            push(&s, atof(token));
-            char *operand = malloc(MAX_TOKEN_SIZE);
-            snprintf(operand, MAX_TOKEN_SIZE, "%s", token);
-            push_expr(&es, operand);
+            empilha(&p, atof(token));
+            char *operando = malloc(MAX_TOKEN_SIZE);
+            snprintf(operando, MAX_TOKEN_SIZE, "%s", token);
+            empilhaExpr(&ep, operando);
         }
     }
 
-    strcpy(infix, pop_expr(&es));
-    return pop(&s);
-}
-
-int main() {
-    char expression[MAX_EXPR_SIZE];
-    char infix[MAX_EXPR_SIZE] = "";
-
-    printf("Digite a expressão em notação pós-fixada: ");
-    fgets(expression, MAX_EXPR_SIZE, stdin);
-    expression[strcspn(expression, "\n")] = '\0';  // Remove newline character if exists
-
-    double result = evaluate_postfix(expression, infix);
-    printf("Resultado: %.2f\n", result);
-    printf("Expressão Infixa: %s\n", infix);
-
-    return 0;
+    strcpy(infixa, desempilhaExpr(&ep));
+    return desempilha(&p);
 }
